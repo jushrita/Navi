4c85224e2eb36af52deac8dda4130e75e3bbadd2
Jushrita <jushrita@Jushritas-MacBook-Pro.local>
2020-09-05 10:52:56 +0530

    first commit

---
+++ Navi/Navi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Navi
+//
+//  Created by Jushrita on 05/09/20.
+//  Copyright © 2020 Jushrita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
